ac66888d873d6732e5cb6d9b76cf065561de8e23
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2023-01-03 12:07:43 +0100

    Update userData.sh

--- userData.sh
+++ userData.sh
@@ -5,4 +5,7 @@ sudo apt install -y ufw
 systemctl start nginx
 systemctl enable nginx
 sudo ufw enable
+sudo ufw allow 'Nginx HTTP'
+sudo ufw allow 'Nginx Full'
+sudo ufw allow 'OpenSSH'
 echo "<h1>I am $(hostname -f)</h1>" > /var/www/html/index.html
